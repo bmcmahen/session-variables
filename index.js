dd7f3044faf44c26f6fbfc3953f9252d05b9cea2
Ben McMahen <ben.mcmahen@gmail.com>
2013-08-06 01:11:35 -0600

    eww tabs

--- index.js
+++ index.js
@@ -10,48 +10,48 @@ var each = require('each');
  */
 
 function Session(){
-	if (!(this instanceof Session)) return new Session();
-	this.attributes = {};
+  if (!(this instanceof Session)) return new Session();
+  this.attributes = {};
 }
 
 Emitter(Session.prototype);
 
 Session.prototype.set = function(key, value, options){
-	options = options || {};
-	var prev = this.attributes[key];
-	this.attributes[key] = value;
-	if (!options.dontStore) store(key, value);
-	if (!options.silent) this.emit('change:'+ key, value, prev);
+  options = options || {};
+  var prev = this.attributes[key];
+  this.attributes[key] = value;
+  if (!options.dontStore) store(key, value);
+  if (!options.silent) this.emit('change:'+ key, value, prev);
 };
 
 Session.prototype.get = function(key){
-	return this.attributes[key];
+  return this.attributes[key];
 };
 
 Session.prototype.unset = function(key){
-	this.set(key, null);
+  this.set(key, null);
 };
 
 Session.prototype.clear = function(){
-	var self = this;
-	each(this.attributes, function(key){
-		self.unset(key);
-	});
+  var self = this;
+  each(this.attributes, function(key){
+    self.unset(key);
+  });
 };
 
 Session.prototype.setDefault = function(key, value, options){
-	if (!this.restored) this.restore();
-	if (!this.attributes[key]) this.set(key, value, options);
-	return this;
+  if (!this.restored) this.restore();
+  if (!this.attributes[key]) this.set(key, value, options);
+  return this;
 };
 
 Session.prototype.restore = function(silent){
-	var self = this;
-	var storage = store();
-	each(storage, function(key, val){
-		self.set(key, val, { dontStore : true });
-	});
-	this.restored = true;
+  var self = this;
+  var storage = store();
+  each(storage, function(key, val){
+    self.set(key, val, { dontStore : true });
+  });
+  this.restored = true;
 };
 
 module.exports = Session;
